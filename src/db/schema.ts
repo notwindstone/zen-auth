89e4db56be572f2dc00b65ec5172bb9aed96b4d7
notwindstone <notwindstone@gmail.com>
2025-02-15 15:00:10 +0300

    feat: implement database code

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { pgTable, text } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users_table', {
+    uuid: text('uuid').primaryKey().unique(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    password: text('password').notNull(),
+});
+
+export type InsertUser = typeof usersTable.$inferInsert;
+export type SelectUser = typeof usersTable.$inferSelect;
