2c3995c457d8efa244aa73bf247ff150abb78234
notwindstone <notwindstone@gmail.com>
2025-02-15 14:32:20 +0300

    refactor: move LoginForm into a separate component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,6 +6,7 @@ import { defaultSession } from "@/lib/sessions";
 import Image from "next/image";
 import Link from "next/link";
 import {REGISTRATION_INPUTS} from "@/app/configs/constants";
+import Login from "@/components/Login/Login";
 
 export function Form() {
     const { session, isLoading } = useSession();
@@ -25,82 +26,10 @@ export function Form() {
         );
     }
 
-    return <LoginForm />;
+    return <Login />;
 }
 
-function LoginForm() {
-    const { login } = useSession();
 
-    return (
-        <div className="bg-white drop-shadow-xl py-4 px-12 rounded-md">
-            <div className="flex flex-col items-center gap-4">
-                <Image
-                    className="mb-4"
-                    width={48}
-                    height={48}
-                    src={nextJsLogo}
-                    alt={"Next.js logo"}
-                />
-                <p className="text-xl font-bold text-black">
-                    Регистрация аккаунта
-                </p>
-                <p className="text-center text-gray-500 font-medium">
-                    Добро пожаловать! Зарегистрируйтесь, чтобы продолжить.
-                </p>
-                <div className="h-[1px] w-full bg-gray-200" />
-                <form
-                    className="w-full flex flex-col gap-4"
-                    onSubmit={function (event) {
-                        event.preventDefault();
-                        const formData = new FormData(event.currentTarget);
-                        const username = formData.get("username") as string;
-                        const email = formData.get("email") as string;
-                        const password = formData.get("password") as string;
-                        login(username, {
-                            optimisticData: {
-                                isLoggedIn: true,
-                                username,
-                                password,
-                            },
-                        });
-                    }}
-                    method="POST"
-                >
-                    {
-                        REGISTRATION_INPUTS.map((currentInput) => {
-                            return (
-                                <div className="flex flex-col gap-2" key={currentInput.name}>
-                                    <p className="font-semibold text-gray-800">
-                                        {currentInput.label}
-                                    </p>
-                                    <input
-                                        className="shadow-sm focus:outline-gray-300 focus:-outline-offset-0 outline-transparent focus:outline-none hover:border-gray-300 border-gray-200 border-[1px] rounded-md px-2 py-1 transition-all"
-                                        type={currentInput.type}
-                                        name={currentInput.name}
-                                        placeholder=""
-                                        required
-                                    />
-                                </div>
-                            );
-                        })
-                    }
-                    <button type="submit">
-                        Продолжить
-                    </button>
-                </form>
-                <p className="text-gray-500 font-medium">
-                    Уже есть аккаунт?{' '}
-                    <Link
-                        className="text-black font-medium"
-                        href={"/auth"}
-                    >
-                        Войдите
-                    </Link>
-                </p>
-            </div>
-        </div>
-    );
-}
 
 function LogoutButton() {
     const {logout} = useSession();
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,80 @@
+import useSession from "@/hooks/use-session";
+import Image from "next/image";
+import nextJsLogo from "../../../public/nextjs-icon.svg";
+import {REGISTRATION_INPUTS} from "@/app/configs/constants";
+import Link from "next/link";
+
+export default function Login() {
+    const { login } = useSession();
+
+    return (
+        <div className="bg-white drop-shadow-xl py-4 px-12 rounded-md">
+            <div className="flex flex-col items-center gap-4">
+                <Image
+                    className="mb-4"
+                    width={48}
+                    height={48}
+                    src={nextJsLogo}
+                    alt={"Next.js logo"}
+                />
+                <p className="text-xl font-bold text-black">
+                    Регистрация аккаунта
+                </p>
+                <p className="text-center text-gray-500 font-medium">
+                    Добро пожаловать! Зарегистрируйтесь, чтобы продолжить.
+                </p>
+                <div className="h-[1px] w-full bg-gray-200" />
+                <form
+                    className="w-full flex flex-col gap-4"
+                    onSubmit={function (event) {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const username = formData.get("username") as string;
+                        const email = formData.get("email") as string;
+                        const password = formData.get("password") as string;
+                        login(username, {
+                            optimisticData: {
+                                isLoggedIn: true,
+                                username,
+                                email,
+                                password,
+                            },
+                        });
+                    }}
+                    method="POST"
+                >
+                    {
+                        REGISTRATION_INPUTS.map((currentInput) => {
+                            return (
+                                <div className="flex flex-col gap-2" key={currentInput.name}>
+                                    <p className="font-semibold text-gray-800">
+                                        {currentInput.label}
+                                    </p>
+                                    <input
+                                        className="shadow-sm focus:outline-gray-300 focus:-outline-offset-0 outline-transparent focus:outline-none hover:border-gray-300 border-gray-200 border-[1px] rounded-md px-2 py-1 transition-all"
+                                        type={currentInput.type}
+                                        name={currentInput.name}
+                                        placeholder=""
+                                        required
+                                    />
+                                </div>
+                            );
+                        })
+                    }
+                    <button type="submit">
+                        Продолжить
+                    </button>
+                </form>
+                <p className="text-gray-500 font-medium">
+                    Уже есть аккаунт?{' '}
+                    <Link
+                        className="text-black font-medium"
+                        href={"/auth"}
+                    >
+                        Войдите
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
+}
