f37b7fefe6728718f47c669640233e6107309c43
notwindstone <notwindstone@gmail.com>
2025-02-19 11:15:46 +0300

    feat: add github repository link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,19 @@ export default function Home() {
     return (
         <main className="p-10">
             <div className="mx-auto flex flex-col gap-4 max-w-lg">
-                <p className="text-2xl font-bold mb-4">
-                    Главная
-                </p>
-                <Form type="sign-up" />
+                <div className="mb-4">
+                    <p className="text-2xl font-bold">
+                        Главная
+                    </p>
+                    <a
+                        target="_blank"
+                        className="text-zinc-500 transition hover:text-black"
+                        href="https://github.com/notwindstone/authless-next-demo"
+                    >
+                        Github репозиторий
+                    </a>
+                </div>
+                <Form type="sign-up"/>
                 <Link
                     className="text-center hover:bg-zinc-700 bg-zinc-800 focus:bg-zinc-600 text-white rounded-md py-2 px-4 transition"
                     href="/protected"
