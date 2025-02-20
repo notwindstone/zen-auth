e52b09e9b01178b607cadc43208ec7304816ff19
notwindstone <notwindstone@gmail.com>
2025-02-20 22:13:33 +0300

    feat: add initial database code

---
+++ src/db/schema.ts
@@ -0,0 +1,22 @@
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("user_table", {
+    id: serial("id").primaryKey()
+});
+
+export const sessionTable = pgTable("session_table", {
+    id: text("id").primaryKey(),
+    userId: integer("user_id")
+        .notNull()
+        .references(() => userTable.id),
+    expiresAt: timestamp("expires_at", {
+        withTimezone: true,
+        mode: "date"
+    }).notNull()
+});
+
+export type InsertUser = typeof userTable.$inferInsert;
+export type SelectUser = typeof userTable.$inferSelect;
+
+export type InsertSession = typeof sessionTable.$inferInsert;
+export type SelectSession = typeof sessionTable.$inferSelect;
