6bf3dcb5d676b0866c7fdd0bb765a36933d8464d
notwindstone <notwindstone@gmail.com>
2025-02-20 21:47:03 +0300

    feat: add initial create-next-app code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import {Form} from "@/components/Form/Form";
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <main className="p-10">
-            <div className="mx-auto flex flex-col gap-4 max-w-lg">
-                <div className="mb-4">
-                    <p className="text-2xl font-bold">
-                        Главная
-                    </p>
-                    <a
-                        target="_blank"
-                        className="text-zinc-500 transition hover:text-black"
-                        href="https://github.com/notwindstone/authless-next-demo"
-                    >
-                        Github репозиторий
-                    </a>
-                </div>
-                <Form type="sign-up"/>
-                <Link
-                    className="text-center hover:bg-zinc-700 bg-zinc-800 focus:bg-zinc-600 text-white rounded-md py-2 px-4 transition"
-                    href="/protected"
-                >
-                    Перейти в профиль
-                </Link>
-            </div>
-        </main>
-    );
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+    </div>
+  )
 }
--- src/types/UserData.type.ts
+++
@@ -1,6 +0,0 @@
-export type UserData = {
-    username?: string;
-    isSignIn?: boolean;
-    email: string;
-    password: string;
-};
