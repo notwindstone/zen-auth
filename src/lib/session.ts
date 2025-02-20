23ba1f921cb2f6127d9e640d15d2f8dc8d320ed0
notwindstone <notwindstone@gmail.com>
2025-02-20 22:30:07 +0300

    feat: implement session id creation

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,9 +1,29 @@
-import { SelectSession, SelectUser } from "@/db/schema";
+import { SelectSession, SelectUser, sessionTable } from "@/db/schema";
+import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
+import { db } from "@/db/db";
+import { sha256 } from "@oslojs/crypto/sha2";
 
 export function generateSessionToken(): string {
+    const bytes = new Uint8Array(20);
+
+    crypto.getRandomValues(bytes);
+
+    return encodeBase32LowerCaseNoPadding(bytes);
+
 }
 
 export async function createSession(token: string, userId: number): Promise<SelectSession> {
+    const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+    const session: SelectSession = {
+        id: sessionId,
+        userId,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
+    };
+    
+    await db.insert(sessionTable).values(session);
+
+    return session;
+
 }
 
 export async function validateSessionToken(token: string): Promise<SessionValidationResult> {
