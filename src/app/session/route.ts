00209222fcfde52d6abb60b78644eaa7e6d1d58c
notwindstone <notwindstone@gmail.com>
2025-02-20 22:51:14 +0300

    feat: add initial routes actions

---
+++ src/app/session/route.ts
@@ -0,0 +1,24 @@
+import { validateSessionToken } from "@/lib/session";
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest): Promise<Response> {
+    const token = request.cookies.get("zen_auth_session")?.value ?? null;
+
+    if (token === null) {
+        return new Response(null, {
+            status: 401
+        });
+    }
+
+    const { session, user } = await validateSessionToken(token);
+
+    if (session === null) {
+        return new Response(null, {
+            status: 401
+        });
+    }
+
+    return new Response(null, {
+        status: 200,
+    });
+}
