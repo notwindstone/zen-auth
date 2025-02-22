aa5f23db9629da9c56156aae713048669a608804
notwindstone <notwindstone@gmail.com>
2025-02-22 12:26:39 +0300

    feat: handle errors in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,23 @@
-"use server";
-
 import { getSession } from "@/lib/routes/session/getSession";
 import { cookies } from "next/headers";
 
 export default async function Page() {
-    const cookie = await cookies();
-    const token = cookie.get('zen_auth_session') ?? null;
+    const cookieStore = await cookies();
+    const token = cookieStore.get('zen_auth_session')?.value ?? null;
     const response = await getSession({
         token,
     });
 
-    console.log(session, user);
+    if (!response.ok) {
+        return (
+            <div>
+                nah mate, your not passing
+            </div>
+        );
+    }
 
     return (
         <div>
-
         </div>
     );
 }
