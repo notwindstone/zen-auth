bddef55c3f867fdf64b0f4d5a891e13729344650
notwindstone <notwindstone@gmail.com>
2025-02-24 16:07:28 +0300

    feat: save code

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,13 +8,14 @@ export const userTable = pgTable("user_table", {
     email: text('email').notNull().unique(),
     password: text('password').notNull(),
     salt: text('salt').notNull(),
-    createdAt: timestamp('createdAt').notNull(),
-    lastSignedIn: timestamp('lastSignedIn').notNull(),
+    createdAt: timestamp('created_at').notNull(),
+    lastSignedIn: timestamp('last_signed_in').notNull(),
 });
 
 export const sessionTable = pgTable("session_table", {
     id: text("id").primaryKey(),
-    lastSignedIn: timestamp("lastSignedIn").notNull(),
+    ipAddress: text("ip_address").notNull(),
+    lastSignedIn: timestamp("last_signed_in").notNull(),
     browser: text("browser").notNull(),
     architecture: text("architecture").notNull(),
     os: text("os").notNull(),
