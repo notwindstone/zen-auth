0fb150fcb7c0d8236225e91af09b50f830066b61
notwindstone <notwindstone@gmail.com>
2025-02-27 22:44:02 +0300

    style: divide long line

--- src/lib/actions/verification.ts
+++ src/lib/actions/verification.ts
@@ -1,5 +1,7 @@
 export function generateVerificationCode(): string {
-    const randomSixDigitNumber = Math.floor(Math.pow(10, 5) + (Math.random() * 9 * Math.pow(10, 5)));
+    const randomSixDigitNumber = Math.floor(
+        Math.pow(10, 5) + (Math.random() * 9 * Math.pow(10, 5)),
+    );
 
     return randomSixDigitNumber.toString();
 }
