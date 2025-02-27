89e9d7504f2bf51e9d4f2d3371e50801bde9281a
notwindstone <notwindstone@gmail.com>
2025-02-27 20:41:40 +0300

    feat: fetch user's public info from db on api request

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,12 +1,32 @@
 import type { NextRequest } from "next/server";
+import { getPublicProfile } from "@/lib/actions/profile";
 
 export async function GET(request: NextRequest): Promise<Response> {
     const searchParams = request.nextUrl.searchParams;
-    const username = searchParams.get('username');
+    const searchUsername = searchParams.get('username') as string;
 
-    console.log(username);
+    const users = await getPublicProfile({ username: searchUsername });
+    const user = users?.[0];
+
+    if (!user) {
+        return new Response(null, {
+            status: 401,
+        });
+    }
+
+    const {
+        username,
+        displayName,
+        avatarUrl,
+        lastSignedIn,
+        createdAt,
+    } = user;
 
     return Response.json({
-        username: username,
+        username,
+        displayName,
+        avatarUrl,
+        lastSignedIn,
+        createdAt,
     });
 }
---
+++ src/lib/actions/profile.ts
@@ -0,0 +1,23 @@
+import { db } from "@/db/db";
+import { SelectUser, userTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function getPublicProfile({
+    username,
+}: {
+    username: string;
+}): Promise<
+        Array<
+            Pick<
+                SelectUser, "username" | "avatarUrl" | "displayName" | "createdAt" | "lastSignedIn"
+            > | undefined
+        >
+> {
+    return (await db.select({
+        username: userTable.username,
+        avatarUrl: userTable.avatarUrl,
+        displayName: userTable.displayName,
+        createdAt: userTable.createdAt,
+        lastSignedIn: userTable.lastSignedIn,
+    }).from(userTable).where(eq(userTable.username, username)));
+}
