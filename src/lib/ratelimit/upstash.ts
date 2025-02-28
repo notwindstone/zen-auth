a3d3c7ed8a910f8e148cc3873c657929adc2b748
notwindstone <notwindstone@gmail.com>
2025-02-28 16:13:03 +0300

    feat: add initial ratelimit

---
+++ src/lib/ratelimit/upstash.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+// Allows 5 requests per 5 seconds
+const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.fixedWindow(5, "5 s"),
+});
