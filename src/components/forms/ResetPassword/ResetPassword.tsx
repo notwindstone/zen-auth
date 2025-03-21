d258a05c977840bf1c80fb655429cb02b21b793b
notwindstone <notwindstone@gmail.com>
2025-03-22 00:04:13 +0300

    feat: implement ResetPassword component logic

--- src/components/forms/ResetPassword/ResetPassword.tsx
+++ src/components/forms/ResetPassword/ResetPassword.tsx
@@ -1,8 +1,18 @@
 "use client";
 
 import { KeyRound } from "lucide-react";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import PasswordInput from "@/components/forms/Inputs/PasswordInput/PasswordInput";
+import AlertBlock from "@/components/misc/AlertBlock/AlertBlock";
+import { useImmer } from "use-immer";
+import { StylesErrorType } from "@/types/UI/StylesError.type";
+import { STYLES_ERROR_INITIAL_DATA, STYLES_ERROR_TYPES } from "@/configs/constants";
+import { useRouter } from "nextjs-toploader/app";
+import { API_REQUEST_METHODS, API_ROUTES } from "@/configs/api";
+import getStylesErrorData from "@/utils/queries/getStylesErrorData";
+import { setSessionTokenCookie } from "@/lib/actions/cookies";
+import { getMonthForwardDate } from "@/utils/misc/getMonthForwardDate";
+import { PAGE_ROUTES } from "@/configs/pages";
 
 export default function ResetPassword({
     token,
@@ -11,18 +21,54 @@ export default function ResetPassword({
     token: string;
     email: string;
 }) {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    const [styles, setStyles] = useImmer<
+        Pick<
+            StylesErrorType,
+            "rtl" | "email" | "password"
+        >
+    >({
+        rtl: STYLES_ERROR_INITIAL_DATA,
+        email: STYLES_ERROR_INITIAL_DATA,
+        password: STYLES_ERROR_INITIAL_DATA,
+    });
+
     async function handlePasswordReset(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
+
+        if (isLoading) {
+            return;
+        }
+
+        setIsLoading(true);
+        setStyles((draft) => {
+            draft.rtl = STYLES_ERROR_INITIAL_DATA;
+            draft.email = STYLES_ERROR_INITIAL_DATA;
+            draft.password = STYLES_ERROR_INITIAL_DATA;
+        });
 
         const formData = new FormData(event.currentTarget);
         const password = formData.get("password");
 
-        if (!password) {
-            alert("Please enter a password");
+        if (!password || !email) {
+            setStyles((draft) => {
+                draft.email = {
+                    error: !Boolean(email),
+                    text: STYLES_ERROR_TYPES.NO_EMAIL,
+                };
+                draft.password = {
+                    error: !Boolean(password),
+                    text: STYLES_ERROR_TYPES.NO_PASSWORD,
+                };
+            });
+            setIsLoading(false);
+
+            return;
         }
 
-        const response = await fetch('/api/reset', {
-            method: "PUT",
+        const resetResponse = await fetch(API_ROUTES.RESET, {
+            method: API_REQUEST_METHODS.PUT,
             body: JSON.stringify({
                 email: email,
                 newPassword: password,
@@ -30,13 +76,63 @@ export default function ResetPassword({
             }),
         });
 
-        if (!response.ok) {
-            alert('Response is not ok');
+        if (!resetResponse.ok) {
+            const { status } = resetResponse;
+            const {
+                rtlError,
+                passwordError,
+            } = getStylesErrorData({
+                status: status,
+                headers: resetResponse.headers,
+            });
+
+            setStyles((draft) => {
+                draft.rtl = rtlError;
+                draft.password = passwordError;
+            });
+            setIsLoading(false);
 
             return;
         }
 
-        alert("Truth.");
+        const loginResponse = await fetch(API_ROUTES.LOGIN, {
+            method: API_REQUEST_METHODS.POST,
+            body: JSON.stringify({
+                login: email,
+                password: password,
+            }),
+        });
+
+        if (!loginResponse.ok) {
+            const { status } = loginResponse;
+            const {
+                rtlError,
+                emailError,
+                passwordError,
+            } = getStylesErrorData({
+                status: status,
+                headers: loginResponse.headers,
+            });
+
+            setStyles((draft) => {
+                draft.rtl = rtlError;
+                draft.email = emailError;
+                draft.password = passwordError;
+            });
+            setIsLoading(false);
+
+            return;
+        }
+
+        const data = await loginResponse.json();
+        const { sessionToken } = data;
+
+        setSessionTokenCookie({
+            token: sessionToken,
+            expiresAt: getMonthForwardDate(),
+        }).then(() => {
+            router.push(PAGE_ROUTES.PROFILE.ROOT);
+        });
     }
 
     return (
@@ -65,13 +161,44 @@ export default function ResetPassword({
                             onSubmit={(event: FormEvent<HTMLFormElement>) => handlePasswordReset(event)}
                             method="POST"
                         >
-                            <PasswordInput isError={false} errorText={""} />
-                            <button
-                                className={`hover:bg-zinc-700 bg-zinc-800 transition mt-2 rounded-md p-2 text-white h-[40px]`}
-                                type="submit"
-                            >
-                                Продолжить
-                            </button>
+                            {
+                                (styles.email.error) && (
+                                    <AlertBlock>
+                                        {styles.email.text}
+                                    </AlertBlock>
+                                )
+                            }
+                            <PasswordInput
+                                isError={styles.password.error}
+                                errorText={styles.password.text}
+                            />
+                            {
+                                (styles.password.error) && (
+                                    <AlertBlock>
+                                        {styles.password.text}
+                                    </AlertBlock>
+                                )
+                            }
+                            {
+                                (styles.rtl.error) && (
+                                    <AlertBlock>
+                                        {styles.rtl.text}
+                                    </AlertBlock>
+                                )
+                            }
+                            {
+                                isLoading ? (
+                                    <div
+                                        className="h-[40px] w-full mt-2 transition animate-pulse bg-zinc-400 rounded-md"/>
+                                ) : (
+                                    <button
+                                        className={`hover:bg-zinc-700 bg-zinc-800 transition mt-2 rounded-md p-2 text-white h-[40px]`}
+                                        type="submit"
+                                    >
+                                        Продолжить
+                                    </button>
+                                )
+                            }
                         </form>
                     </div>
                 </div>
