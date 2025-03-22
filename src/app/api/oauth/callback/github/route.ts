281c6dbe99fd657e54dc4e648e7e39798e5fff6d
notwindstone <notwindstone@gmail.com>
2025-03-22 22:15:49 +0300

    feat: oauth2 thinks hes him (github callback checks)

--- src/app/api/oauth/callback/github/route.ts
+++ src/app/api/oauth/callback/github/route.ts
@@ -1,16 +1,27 @@
 import { NextRequest } from "next/server";
 import * as arctic from "arctic";
+import { cookies } from "next/headers";
+import {API_STATUS_CODES} from "@/configs/api";
 
 export async function GET(request: NextRequest): Promise<Response> {
     let tokens;
     let accessToken;
 
+    const cookieStore = await cookies();
     const github = new arctic.GitHub(
         process.env.GITHUB_CLIENT_ID!,
         process.env.GITHUB_SECRET_KEY!,
         null,
     );
-    const code = request.nextUrl.searchParams.get("code");
+    const code = request.nextUrl.searchParams.get("code") as string;
+    const state = request.nextUrl.searchParams.get("state") as string;
+    const storedState = cookieStore.get('state')?.value as string;
+
+    if (code === null || storedState === null || state !== storedState) {
+        return new Response(null, {
+            status: API_STATUS_CODES.ERROR.BAD_REQUEST,
+        });
+    }
 
     try {
         tokens = await github.validateAuthorizationCode(code ?? "");
@@ -30,7 +41,16 @@ export async function GET(request: NextRequest): Promise<Response> {
         // Parse error
     }
 
+    const response = await fetch("https://api.github.com/user", {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    const user = await response.json();
+
+
     return Response.json({
         accessToken: accessToken,
+        user: user,
     });
 }
