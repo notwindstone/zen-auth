30be1e0b23c3be4f1edaf470af5b7334dd95a8e3
notwindstone <notwindstone@gmail.com>
2025-03-22 22:11:24 +0300

    feat: oauth2 is the main character (github callback initial code)

--- src/app/api/oauth/callback/github/route.ts
+++ src/app/api/oauth/callback/github/route.ts
@@ -1,5 +1,36 @@
 import { NextRequest } from "next/server";
+import * as arctic from "arctic";
 
 export async function GET(request: NextRequest): Promise<Response> {
+    let tokens;
+    let accessToken;
 
+    const github = new arctic.GitHub(
+        process.env.GITHUB_CLIENT_ID!,
+        process.env.GITHUB_SECRET_KEY!,
+        null,
+    );
+    const code = request.nextUrl.searchParams.get("code");
+
+    try {
+        tokens = await github.validateAuthorizationCode(code ?? "");
+        accessToken = tokens.accessToken();
+    } catch (e) {
+        if (e instanceof arctic.OAuth2RequestError) {
+            // Invalid authorization code, credentials, or redirect URI
+            const code = e.code;
+            // ...
+            console.error(code);
+        }
+        if (e instanceof arctic.ArcticFetchError) {
+            // Failed to call `fetch()`
+            const cause = e.cause;
+            console.error(cause);
+        }
+        // Parse error
+    }
+
+    return Response.json({
+        accessToken: accessToken,
+    });
 }
