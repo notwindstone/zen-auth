b06f668129d5533c595f45b6f82da2c66b800910
notwindstone <notwindstone@gmail.com>
2025-03-23 21:55:28 +0300

    feat: show google oauth2 button as disabled

--- src/components/forms/OAuth2Providers/OAuth2Providers.tsx
+++ src/components/forms/OAuth2Providers/OAuth2Providers.tsx
@@ -10,9 +10,9 @@ export default function OAuth2Providers() {
 
     return (
         <>
-            <a
-                className="min-w-[40%] flex-1 border-gray-200 h-8 flex justify-center items-center text-zinc-800 font-medium border-[1px] rounded-md hover:bg-zinc-100 transition text-sm"
-                href="/api/oauth/login/google"
+            <div
+                className="select-none min-w-[40%] flex-1 border-gray-200 h-8 flex justify-center items-center text-zinc-400 font-medium border-[1px] rounded-md bg-gray-200 transition text-sm"
+                //href="/api/oauth/login/google"
             >
                 <div className="flex flex-nowrap items-center gap-2">
                     <SiGoogle/>
@@ -20,7 +20,7 @@ export default function OAuth2Providers() {
                         Google
                     </p>
                 </div>
-            </a>
+            </div>
             <a
                 className="min-w-[40%] flex-1 border-gray-200 h-8 flex justify-center items-center text-zinc-800 font-medium border-[1px] rounded-md hover:bg-zinc-100 transition text-sm"
                 href={"/api/oauth/login/github?error_url=" + pathname}
