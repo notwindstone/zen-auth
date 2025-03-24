147652f5fc9bf5d2a2fce3c05a135298fad11db3
notwindstone <notwindstone@gmail.com>
2025-03-24 14:14:33 +0300

    style: fix typo

--- src/app/oauth_different_browser/page.tsx
+++ src/app/oauth_different_browser/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return (
         <div className="flex flex-col gap-2">
             <div className="text-sm text-zinc-400">
-                Произошла ошибка при авторизации через OAuth2, связанная с отсутствие токена сессии в cookies.
+                Произошла ошибка при авторизации через OAuth2, связанная с отсутствием токена сессии в cookies.
             </div>
             <div className="text-sm text-zinc-400">
                 Возможно, вы начали процесс входа в одном браузере (например, через встроенный в телеграм), а закончили в другом (например, открыли страницу OAuth2 провайдера в Firefox или Chrome).
