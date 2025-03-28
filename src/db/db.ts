468c3b8dc1f6113b5c4c55f1931f6a9580f8750d
notwindstone <notwindstone@gmail.com>
2025-03-28 10:39:16 +0300

    feat: implement support for local postgres database

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,23 @@
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as neonDriver } from "drizzle-orm/neon-http";
+import { drizzle as postgresDriver } from "drizzle-orm/postgres-js";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 
 config({ path: ".env.local" });
 
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql });
+let driver;
+
+switch (process.env.DB_TYPE!.toLowerCase()) {
+    case "postgres":
+        driver = postgresDriver(process.env.DATABASE_URL!);
+
+        break;
+    case "neon":
+    default:
+        const sql = neon(process.env.DATABASE_URL!);
+        driver = neonDriver({ client: sql });
+
+        break;
+}
+
+export const db = driver;
