3fddf2397bb7ee043c295714f2fa0af98f046fde
notwindstone <notwindstone@gmail.com>
2025-03-31 17:36:59 +0300

    fix: handle x-forwarded-for header better

--- src/utils/secure/getIpAddress.ts
+++ src/utils/secure/getIpAddress.ts
@@ -1,5 +1,11 @@
 import { NextRequest } from "next/server";
 
 export function getIpAddress(request: NextRequest): string {
-    return (request.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')?.[0];
+    const requestIP = request.headers.get('x-forwarded-for');
+
+    if (!requestIP || requestIP === "::ffff:127.0.0.1") {
+        return '127.0.0.1';
+    }
+
+    return requestIP.split(',')?.[0];
 }
