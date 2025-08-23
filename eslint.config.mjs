53e94821646ef19ca879ded3a2366ac87c3daf03
notwindstone <notwindstone@gmail.com>
2025-08-23 20:37:06 +0300

    style: fix eslint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import {dirname} from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
@@ -6,24 +6,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends(
-      "next/core-web-vitals",
-      "next/typescript",
-  ),
-  {
-    rules: {
-      semi: "error",
-      indent: ["error", 4, { "SwitchCase": 1 }],
-      curly: "error",
-      "object-curly-spacing": ["error", "always"],
-      "comma-dangle": ["error", "always-multiline"],
-      "arrow-parens": ["error", "always"],
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+    ),
+    {
+        rules: {
+            semi: "error",
+            indent: ["error", 4, { "SwitchCase": 1 }],
+            curly: "error",
+            "object-curly-spacing": ["error", "always"],
+            "comma-dangle": ["error", "always-multiline"],
+            "arrow-parens": ["error", "always"],
+        },
     },
-  },
 ];
 
 export default eslintConfig;
